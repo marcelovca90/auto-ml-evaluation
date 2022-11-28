7692beb2859ef47cfc424eed7d6aee8181adb174
automl <automl>
2022-11-28 00:23:51 -0300

    Add experimental support for multilabel

--- automl_autokeras.py
+++ automl_autokeras.py
@@ -6,14 +6,19 @@ try:
 
     X_train, X_test, y_train, y_test = load_data_delegate()
 
-    autokeras = ak.StructuredDataClassifier(max_trials=1, overwrite=True, seed=SEED)
+    autokeras = ak.StructuredDataClassifier(multi_label=True, max_trials=3, overwrite=True, seed=SEED)
 
     TIMER.tic()
-    autokeras.fit(X_train, y_train, epochs=100)
+    autokeras.fit(X_train, y_train, epochs=1000)
     training_time = TIMER.tocvalue()
 
     TIMER.tic()
-    y_pred = autokeras.predict(X_test).astype(int).flatten()
+    # multilabel datasets
+    if get_dataset_ref() in [41465, 41468, 42470, 41471, 41473]:
+        y_pred = autokeras.predict(X_test).astype(int)
+    # binary and multiclass datasets
+    else:
+        y_pred = autokeras.predict(X_test).astype(int).flatten()
     test_time = TIMER.tocvalue()
 
     collect_and_persist_results(y_test, y_pred, training_time, test_time, "autokeras")
--- common.py
+++ common.py
@@ -9,7 +9,7 @@ from sklearn.datasets import fetch_openml
 from sklearn.metrics import *
 from sklearn.model_selection import train_test_split
 
-EXEC_TIME_MINUTES = 1
+EXEC_TIME_MINUTES = 10
 EXEC_TIME_SECONDS = EXEC_TIME_MINUTES*60
 SEED = 42
 TIMER = TicToc()
@@ -17,7 +17,7 @@ TIMER = TicToc()
 def get_dataset_ref():
     dataset_ref = None
     if len(sys.argv) != 2:
-        print('usage: python common.py dataset_ref; using default dataset (61 - iris)')
+        print('usage: python common.py dataset_ref')
     else:
         try:
             dataset_ref = int(sys.argv[1])
@@ -28,7 +28,7 @@ def get_dataset_ref():
 def infer_task_type(y_test):
     num_classes = len(set(y_test))
     if num_classes == 1:
-        raise Exception('Malformed data set; num_classes = 1')
+        raise Exception('Malformed data set; num_classes == 1')
     elif num_classes == 2:
         task_type = 'binary'
     else:
@@ -55,7 +55,14 @@ def load_csv():
 
 def load_openml():
     dataset = fetch_openml(data_id=get_dataset_ref(), return_X_y=False)
-    X, y = dataset.data, pd.Series(pd.factorize(dataset.target)[0])
+     # multilabel datasets
+    if get_dataset_ref() in [41465, 41468, 42470, 41471, 41473]:
+        X, y = dataset.data, dataset.target
+        for col in y.columns.values:
+            y[col] = y[col].map({'FALSE': 0, 'TRUE': 1}).to_numpy()
+    # binary and multiclass datasets
+    else:
+        X, y = dataset.data, pd.Series(pd.factorize(dataset.target)[0])
     return train_test_split(X, y, test_size=0.2, random_state=SEED)
 
 def calculate_score(metric, y_true, y_pred, **kwargs):
@@ -65,23 +72,26 @@ def calculate_score(metric, y_true, y_pred, **kwargs):
 		return -1.0
 
 def collect_and_persist_results(y_test, y_pred, training_time, test_time, framework="unknown"):
-  results = {}
-  results.update({"framework":               framework})
-  results.update({"accuracy_score":          calculate_score(accuracy_score, y_test, y_pred)})
-  results.update({"average_precision_score": calculate_score(average_precision_score, y_test, y_pred)})
-  results.update({"balanced_accuracy_score": calculate_score(balanced_accuracy_score, y_test, y_pred)})
-  results.update({"cohen_kappa_score":       calculate_score(cohen_kappa_score, y_test, y_pred)})
-  results.update({"f1_score_macro":          calculate_score(f1_score, y_test, y_pred, average="macro")})
-  results.update({"f1_score_micro":          calculate_score(f1_score, y_test, y_pred, average="micro")})
-  results.update({"f1_score_weighted":       calculate_score(f1_score, y_test, y_pred, average="weighted")})
-  results.update({"matthews_corrcoef":       calculate_score(matthews_corrcoef, y_test, y_pred)})
-  results.update({"precision_score":         calculate_score(precision_score, y_test, y_pred)})
-  results.update({"recall_score":            calculate_score(recall_score, y_test, y_pred)})
-  results.update({"roc_auc_score":           calculate_score(roc_auc_score, y_test, y_pred)})
-  results.update({"training_time":           time.strftime("%H:%M:%S", time.gmtime(training_time))})
-  results.update({"test_time":               time.strftime("%H:%M:%S", time.gmtime(test_time))})
-  print(results)
-  if not os.path.exists(f'./results/{get_dataset_ref()}'):
-    os.makedirs(f'./results/{get_dataset_ref()}')
-  with open(f"./results/{get_dataset_ref()}/automl_{framework}.json", "w") as outfile:
-    json.dump(results, outfile)
+    results = {}
+    results.update({"framework":                                framework})
+    results.update({"accuracy_score":                           calculate_score(accuracy_score, y_test, y_pred)})
+    results.update({"average_precision_score":                  calculate_score(average_precision_score, y_test, y_pred)})
+    results.update({"balanced_accuracy_score":                  calculate_score(balanced_accuracy_score, y_test, y_pred)})
+    results.update({"cohen_kappa_score":                        calculate_score(cohen_kappa_score, y_test, y_pred)})
+    results.update({"f1_score_macro":                           calculate_score(f1_score, y_test, y_pred, average="macro")})
+    results.update({"f1_score_micro":                           calculate_score(f1_score, y_test, y_pred, average="micro")})
+    results.update({"f1_score_weighted":                        calculate_score(f1_score, y_test, y_pred, average="weighted")})
+    results.update({"matthews_corrcoef":                        calculate_score(matthews_corrcoef, y_test, y_pred)})
+    results.update({"precision_score":                          calculate_score(precision_score, y_test, y_pred)})
+    results.update({"recall_score":                             calculate_score(recall_score, y_test, y_pred)})
+    results.update({"roc_auc_score":                            calculate_score(roc_auc_score, y_test, y_pred)})
+    results.update({"coverage_error":                           calculate_score(coverage_error, y_test, y_pred)})
+    results.update({"label_ranking_average_precision_score":    calculate_score(label_ranking_average_precision_score, y_test, y_pred)})
+    results.update({"label_ranking_loss":                       calculate_score(label_ranking_loss, y_test, y_pred)})
+    results.update({"training_time":                            time.strftime("%H:%M:%S", time.gmtime(training_time))})
+    results.update({"test_time":                                time.strftime("%H:%M:%S", time.gmtime(test_time))})
+    print(results)
+    if not os.path.exists(f'./results/{get_dataset_ref()}'):
+        os.makedirs(f'./results/{get_dataset_ref()}')
+    with open(f"./results/{get_dataset_ref()}/automl_{framework}.json", "w") as outfile:
+        json.dump(results, outfile)
