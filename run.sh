f2ed0d5e45ff278c1faa7aa1cb94c70956b8dc4c
marcelovca90 <marcelovca90@gmail.com>
2024-01-04 05:50:08 -0300

    Undo comments

--- run.sh
+++ run.sh
@@ -40,7 +40,7 @@ rm -rf Autogluon* &> /dev/null
 rf -rf gama_* &> /dev/null
 rm -rf structured* &> /dev/null
 rm -rf results/* &> /dev/null
-# rm -rf venv-* &> /dev/null
+rm -rf venv-* &> /dev/null
 rm *.log &> /dev/null
 echo Finished cleaning files from previous executions at $(date).
 
@@ -49,60 +49,60 @@ for id in ${datasets[@]}; do
     echo ======== Processing ========
     echo Started processing dataset $id at $(date).
 
-    AutoGluon
+    # AutoGluon
     echo ======== AutoGluon ========
     python3.8 -m venv venv-autogluon
     source ./venv-autogluon/bin/activate
-    # python3.8 -m pip install --upgrade pip
-    # python3.8 -m pip install --upgrade setuptools pytictoc wheel pandas scikit-learn torch==1.12+cpu torchvision==0.13.0+cpu torchtext==0.13.0 -f https://download.pytorch.org/whl/cpu/torch_stable.html autogluon
+    python3.8 -m pip install --upgrade pip
+    python3.8 -m pip install --upgrade setuptools pytictoc wheel pandas scikit-learn torch==1.12+cpu torchvision==0.13.0+cpu torchtext==0.13.0 -f https://download.pytorch.org/whl/cpu/torch_stable.html autogluon
     python3.8 ./automl_autogluon.py $id
 
     # AutoKeras
     echo ======== AutoKeras ========
     python3.8 -m venv venv-autokeras
     source ./venv-autokeras/bin/activate
-    # python3.8 -m pip install --upgrade pip
-    # python3.8 -m pip install --upgrade setuptools pytictoc wheel pandas scikit-learn git+https://github.com/keras-team/keras-tuner.git autokeras
+    python3.8 -m pip install --upgrade pip
+    python3.8 -m pip install --upgrade setuptools pytictoc wheel pandas scikit-learn git+https://github.com/keras-team/keras-tuner.git autokeras
     python3.8 ./automl_autokeras.py $id
 
     # AutoPyTorch
     echo ======== AutoPyTorch ========
     python3.8 -m venv venv-autopytorch
     source ./venv-autopytorch/bin/activate
-    # python3.8 -m pip install --upgrade pip
-    # python3.8 -m pip install --upgrade setuptools pytictoc wheel pandas scikit-learn swig torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu autoPyTorch
+    python3.8 -m pip install --upgrade pip
+    python3.8 -m pip install --upgrade setuptools pytictoc wheel pandas scikit-learn swig torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu autoPyTorch
     python3.8 ./automl_autopytorch.py $id
 
     # AutoSklearn
     echo ======== AutoSklearn ========
     python3.8 -m venv venv-autosklearn
     source ./venv-autosklearn/bin/activate
-    # python3.8 -m pip install --upgrade pip
-    # python3.8 -m pip install --upgrade setuptools pytictoc wheel pandas scikit-learn auto-sklearn
+    python3.8 -m pip install --upgrade pip
+    python3.8 -m pip install --upgrade setuptools pytictoc wheel pandas scikit-learn auto-sklearn
     python3.8 ./automl_autosklearn.py $id
 
     # EvalML
     echo ======== EvalML ========
     python3.8 -m venv venv-evalml
     source ./venv-evalml/bin/activate
-    # python3.8 -m pip install --upgrade pip
-    # python3.8 -m pip install --upgrade setuptools pytictoc wheel pandas scikit-learn evalml
+    python3.8 -m pip install --upgrade pip
+    python3.8 -m pip install --upgrade setuptools pytictoc wheel pandas scikit-learn evalml
     python3.8 ./automl_evalml.py $id
 
     # FLAML
     echo ======== FLAML ========
     python3.8 -m venv venv-flaml
     source ./venv-flaml/bin/activate
-    # python3.8 -m pip install --upgrade pip
-    # python3.8 -m pip install --upgrade setuptools pytictoc wheel pandas scikit-learn flaml[automl]
+    python3.8 -m pip install --upgrade pip
+    python3.8 -m pip install --upgrade setuptools pytictoc wheel pandas scikit-learn flaml[automl]
     python3.8 ./automl_flaml.py $id
 
     # GAMA
     echo ======== GAMA ========
     python3.8 -m venv venv-gama
     source ./venv-gama/bin/activate
-    # python3.8 -m pip install --upgrade pip
-    # python3.8 -m pip install --upgrade setuptools pytictoc wheel pandas scikit-learn gama
+    python3.8 -m pip install --upgrade pip
+    python3.8 -m pip install --upgrade setuptools pytictoc wheel pandas scikit-learn gama
     sed -i 's/ SCORERS/ _SCORERS/' ./venv-gama/lib/python3.8/site-packages/gama/utilities/metrics.py
     python3.8 ./automl_gama.py $id
 
@@ -110,24 +110,24 @@ for id in ${datasets[@]}; do
     echo ======== H2O ========
     python3.8 -m venv venv-h2o
     source ./venv-h2o/bin/activate
-    # python3.8 -m pip install --upgrade pip
-    # python3.8 -m pip install --upgrade setuptools pytictoc wheel pandas scikit-learn requests tabulate future h2o
+    python3.8 -m pip install --upgrade pip
+    python3.8 -m pip install --upgrade setuptools pytictoc wheel pandas scikit-learn requests tabulate future h2o
     python3.8 ./automl_h2o.py $id
 
     # LightAutoML
     echo ======== LightAutoML ========
     python3.8 -m venv venv-lightautoml
     source ./venv-lightautoml/bin/activate
-    # python3.8 -m pip install --upgrade pip
-    # python3.8 -m pip install --upgrade setuptools pytictoc wheel pandas scikit-learn lightautoml
+    python3.8 -m pip install --upgrade pip
+    python3.8 -m pip install --upgrade setuptools pytictoc wheel pandas scikit-learn lightautoml
     python3.8 ./automl_lightautoml.py $id
 
     TPOT
     echo ======== TPOT ========
     python3.8 -m venv venv-tpot
     source ./venv-tpot/bin/activate
-    # python3.8 -m pip install --upgrade pip
-    # python3.8 -m pip install --upgrade setuptools pytictoc wheel pandas scikit-learn deap update_checker tqdm stopit xgboost torch tpot
+    python3.8 -m pip install --upgrade pip
+    python3.8 -m pip install --upgrade setuptools pytictoc wheel pandas scikit-learn deap update_checker tqdm stopit xgboost torch tpot
     python3.8 ./automl_tpot.py $id
 
     echo Finished processing dataset $id at $(date).
